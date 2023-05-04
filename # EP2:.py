f9b977223015ace013a0ec4e329120810f55b36a
Lucasgrillo13 <116032744+Lucasgrillo13@users.noreply.github.com>
2023-05-04 01:53:39 -0300

    Update # EP2:.py

--- # EP2:.py
+++ # EP2:.py
@@ -32,3 +32,19 @@ def posiciona_frota(frota):
                 tabuleiro[linha][coluna] = 1
 
     return tabuleiro
+def afundados(frota, tabuleiro):
+    afundados = 0
+
+
+
+     for navio, posicoes in frota.items():
+        for posicao in posicoes:
+            afundado = True
+            for coordenada in posicao:
+                x, y = coordenada
+                if tabuleiro[x][y] != "X":
+                    afundado = False
+                    break
+            if afundado:
+                afundados += 1
+    return afundados
